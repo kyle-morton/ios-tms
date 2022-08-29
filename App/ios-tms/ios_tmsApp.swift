0c9ae4f9769ee7d103ccff1f185fb07935987396
Kyle Morton <kylemortondevelopment@gmail.com>
2022-08-28 21:12:23 -0500

    init for app and api

---
+++ App/ios-tms/ios_tmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tmsApp.swift
+//  ios-tms
+//
+//  Created by Kyle Morton on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_tmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
